9f790c6a652b5bdf594c39a684f521ffeeb85394
canarinho <devcanarinho@proton.me>
2022-10-02 18:03:08 -0300

    test: separate mocks

--- __tests__/action.test.ts
+++ __tests__/action.test.ts
@@ -1,36 +1,10 @@
 import * as core from '@actions/core';
 import { action } from '../src/action';
-
-const mockGetProject = {
-  paging: {
-    pageIndex: 1,
-    pageSize: 100,
-    total: 2,
-  },
-  components: [
-    {
-      organization: 'my-org-1',
-      key: 'project-key-1',
-      name: 'Project Name 1',
-      qualifier: 'TRK',
-      visibility: 'public',
-      lastAnalysisDate: '2017-03-01T11:39:03+0300',
-      revision: 'cfb82f55c6ef32e61828c4cb3db2da12795fd767',
-    },
-    {
-      organization: 'my-org-1',
-      key: 'project-key-2',
-      name: 'Project Name 1',
-      qualifier: 'TRK',
-      visibility: 'private',
-      lastAnalysisDate: '2017-03-02T15:21:47+0300',
-      revision: '7be96a94ac0c95a61ee6ee0ef9c6f808d386a355',
-    },
-  ],
-};
-
-const mockAxiosGet = jest.fn();
-const mockAxiosPost = jest.fn();
+import {
+  mockAxiosGet,
+  mockAxiosPost,
+  mockGetProjectResponse,
+} from './test-utils';
 
 jest.mock('axios', () => ({
   create: () => ({
@@ -47,6 +21,10 @@ jest.mock('@actions/github', () => ({
     },
   },
 }));
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 test('Fails if inputs are wrong or missing', async () => {
   const spyGetInput = jest
@@ -68,7 +46,7 @@ test('Continue if project already exists', async () => {
 
   mockAxiosGet.mockResolvedValueOnce({
     config: { method: 'GET', url: '' },
-    data: mockGetProject,
+    data: mockGetProjectResponse,
   });
 
   const exitCode = await action.run();
